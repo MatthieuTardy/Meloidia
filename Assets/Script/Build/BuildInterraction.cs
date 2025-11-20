eed94a7251165a2c4235459eeea067a6430399bf
Flamme_Vire <noahcoevoet@gmail.com>
2025-11-20 15:32:19 +0100

    build Trigger (don't work)

--- Assets/Script/Build/BuildInterraction.cs
+++ Assets/Script/Build/BuildInterraction.cs
@@ -8,6 +8,7 @@ public class BuildInterraction : MonoBehaviour
 
 
     [SerializeField] bool PlayerIsBuildMode;
+    [SerializeField] KeyCode buildKey;
     bool previewInstantiate;
     [Range(1,10)][SerializeField] int Ratio;
 
@@ -15,15 +16,21 @@ public class BuildInterraction : MonoBehaviour
     GameObject construct;
     [SerializeField] Transform constructionSpawn;
     Transform Spawn;
+    
     private void Update()
     {
         if (PlayerIsBuildMode)
         {
             InstantiatePreViewBuild();
             PrevisualizeConstruct();
+            if (Input.GetKeyDown(buildKey))
+            {
+                TryToBuild();
+            }
         }
     }
 
+    #region Build Preview
     void PrevisualizeConstruct()
     {
         construct.transform.position = SetOnGrid(Ratio);
@@ -50,5 +57,32 @@ public class BuildInterraction : MonoBehaviour
             Mathf.RoundToInt(pos.y),
             Mathf.RoundToInt(pos.z / gridSize) * (int)gridSize
         );
+    }
+    #endregion
+
+    void TryToBuild()
+    {
+        if (CanBuild())
+        {
+            Debug.Log("BUILDED");
+        }
+    }
+
+        bool m_HitDetect;
+        RaycastHit m_Hit;
+    bool CanBuild()
+    {
+
+        //Test to see if there is a hit using a BoxCast
+        //Calculate using the center of the the GameObject's rotation, and the maximum distance as variables.
+        //Also fetch the hit data
+
+        m_HitDetect = Physics.BoxCast(Spawn.position,(Spawn.localScale*Ratio)*0.5f,Spawn.position,out m_Hit,Spawn.rotation,1);
+        if (m_HitDetect)
+        {
+            //Output the name of the Collider your Box hit
+            Debug.Log("Hit : " + m_Hit.collider.name);
+        }
+        return true;
     }
 }
