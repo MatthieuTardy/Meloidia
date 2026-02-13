26b894f66be50aa5552c214634b109ce4dcd8880
flammeVire <noahcoevoet@gmail.com>
2026-02-13 18:39:13 +0100

    TreeNeed

---
+++ Assets/Script/TreeNeed.cs
@@ -0,0 +1,104 @@
+using NaughtyAttributes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TreeNeed : MonoBehaviour
+{
+    [SerializeField] NeedCondition[] Needs;
+    int currentIndex;
+
+    public void CheckNeed()
+    {
+        if (Needs[currentIndex] != null)
+        {
+            if (Needs[currentIndex].WantCN)
+            {
+                int CN1 = 0;
+                int CN2 = 0;
+                int CN3 = 0;
+                int CN4 = 0;
+                int CN5 = 0;
+
+                foreach (var i in Needs[currentIndex].typeOfCN)
+                {
+                    if (i == LegumeManager.type.un)
+                    {
+                        CN1++;
+                    }
+                    else if (i == LegumeManager.type.deux)
+                    {
+                        CN2++;
+                    }
+                    else if (i == LegumeManager.type.trois)
+                    {
+                        CN3++;
+                    }
+                    else if (i == LegumeManager.type.quatre)
+                    {
+                        CN4++;
+                    }
+                    else if (i == LegumeManager.type.cinq)
+                    {
+                        CN5++;
+                    }
+                }
+
+                int CNT1 = 0;
+                int CNT2 = 0;
+                int CNT3 = 0;
+                int CNT4 = 0;
+                int CNT5 = 0;
+                foreach (var i in GameManager.Instance.legumeManagerList)
+                {
+                    if (i.legumeType == LegumeManager.type.un)
+                    {
+                        CNT1++;
+                    }
+                    else if (i.legumeType == LegumeManager.type.deux)
+                    {
+                        CNT2++;
+                    }
+                    else if (i.legumeType == LegumeManager.type.trois)
+                    {
+                        CNT3++;
+                    }
+                    else if (i.legumeType == LegumeManager.type.quatre)
+                    {
+                        CNT4++;
+                    }
+                    else if (i.legumeType == LegumeManager.type.cinq)
+                    {
+                        CNT5++;
+                    }
+                }
+
+                bool pass = false;
+                if (CN1 != 0 && CNT1 >= CN1) 
+                {
+                    pass = true;
+                }
+                if(CN2 != 0 && CNT2 >= CN2)
+                {
+                        pass = true;
+                }
+            }
+        }
+    }
+}
+
+
+[Serializable]
+class NeedCondition
+{
+    [InfoBox("ajouter autant de CrocNote et de ressources que nécessaire")]
+    public bool WantCN;
+    public bool WantRessources;
+
+    public UnityEvent OnAllNeed;
+
+    public LegumeManager.type[] typeOfCN;
+    public TypeOfRessources[] typeOfRessources;
+}
